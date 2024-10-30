cc72c0d12430fd7f0c887ae5b38fbf9967ddc889
felipe.dev <083felype@gmail.com>
2024-10-30 15:39:25 -0300

    feat: add animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    Aos.init({
+    });
+  }
 }
